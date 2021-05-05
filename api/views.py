ddd3a32959c89026ce3096d9013cfea42d2e566c
Olamide <olowofesovictor@gmail.com>
2021-05-05 13:52:23 +0100

    post text to api

--- api/views.py
+++ api/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
 from art import *
 
+
+@csrf_exempt
 def index(request):
-    return HttpResponse(f"{tprint('Victor', font='cola')}")
+
+    text = request.POST.get("text")
+
+    return HttpResponse(f"{text2art('{text}', font='black')}")
